962a09de2d5b98c4b8bc285b99dfb8a67860be24
nature <simjayeon98@gmail.com>
2025-01-10 01:11:35 +0900

    [Dev] 상품 조회 API 구현

---
+++ src/main/java/kr/hhplus/be/server/domain/service/ProductService.java
@@ -0,0 +1,44 @@
+package kr.hhplus.be.server.domain.service;
+
+import kr.hhplus.be.server.domain.repository.ProductRepositoryImpl;
+import kr.hhplus.be.server.dto.response.ProductResponse;
+import kr.hhplus.be.server.entity.Product;
+import kr.hhplus.be.server.support.exception.ErrorCode;
+import kr.hhplus.be.server.support.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepositoryImpl productRepositoryImpl;
+
+    public ResponseEntity<List<ProductResponse>> getProducts(int page, int size) {
+
+        // 상품 조회
+        int offset = page * size;
+        List<Product> products = productRepositoryImpl.searchByPagination(offset, size);
+
+        // 상품이 없을 경우 예외 처리
+        if (products.isEmpty()) {
+            throw new NotFoundException(ErrorCode.NOT_FOUND.getMessage());
+        }
+
+        // Entity > DTO 변환
+        List<ProductResponse> responses = products.stream()
+                .map(ProductResponse::of)
+                .toList();
+
+        return ResponseEntity.ok(responses);
+    }
+
+    public ResponseEntity<List<ProductResponse>> getTop5Products() {
+        // TODO : 인기 상품 조회 로직 추가
+
+        return null;
+    }
+}
--- src/main/java/kr/hhplus/be/server/dto/response/ProductResponse.java
+++ src/main/java/kr/hhplus/be/server/dto/response/ProductResponse.java
@@ -1,10 +1,22 @@
 package kr.hhplus.be.server.dto.response;
 
 
+import kr.hhplus.be.server.entity.Product;
+import lombok.Builder;
+
+@Builder
 public record ProductResponse(
         Long productId,
         String productName,
         Integer productPrice,
         Integer productQuantity
 ) {
+    public static ProductResponse of(Product product) {
+        return ProductResponse.builder()
+                .productId(product.getId())
+                .productName(product.getName())
+                .productPrice(product.getPrice())
+                .productQuantity(product.getQuantity())
+                .build();
+    }
 }
