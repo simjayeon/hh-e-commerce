acad671dc590ffbb242cac972b4befb7d15e779a
nature <simjayeon98@gmail.com>
2025-01-16 22:25:21 +0900

    [DEV] yml 설정 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,21 +39,20 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-integration")
     implementation("org.springframework.integration:spring-integration-jdbc")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
 
     // Querydsl
     implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
-//    annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jpa")
     annotationProcessor("jakarta.persistence:jakarta.persistence-api")
     annotationProcessor("jakarta.annotation:jakarta.annotation-api")
-    annotationProcessor ("com.querydsl:querydsl-apt:${dependencyManagement.importedProperties["querydsl.version"]}:jakarta")
+    annotationProcessor("com.querydsl:querydsl-apt:${dependencyManagement.importedProperties["querydsl.version"]}:jakarta")
 
     // DB
     runtimeOnly("com.mysql:mysql-connector-j")
-    implementation ("com.h2database:h2")
-
+    implementation("com.h2database:h2")
 
     // Test
-    testImplementation ("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.testcontainers:junit-jupiter")
@@ -61,8 +60,8 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // Swagger
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
-    implementation ("org.springdoc:springdoc-openapi-ui:1.7.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.3")
+    implementation("org.springdoc:springdoc-openapi-ui:1.7.0")
 
     // Lombok
     compileOnly("org.projectlombok:lombok")
